6579018b47ed4e08e345838e23de66241e7c92d1
Adarsh Pandey <pandeyadarsh6206@gmail.com>
2025-02-08 20:48:59 +0530

    add refresh Token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,20 @@ import {uploadOnCLoudinary} from "../utils/cloudinary.js"
 import { apiResponse } from "../utils/apiResponse.js";
 
 
+const generateAccessAndRefreshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+        return {accessToken, refreshToken}
+    } catch (error) {
+        throw new apiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
+
 const registerUser=asyncHandler(async(req,res)=>{
     // res.status(400).json({
     //     message: "ok"
